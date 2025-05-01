d1d46008e8b99f7bfc2ba47e3b140d74251499c7
code3743 <jota.lopez@correounivalle.edu.co>
2025-05-01 07:03:42 -0500

    Implement JWT authentication filter and route configuration for API gateway

---
+++ src/main/java/com/gtu/api_gateway/config/RouteConfig.java
@@ -0,0 +1,46 @@
+package com.gtu.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.gtu.api_gateway.security.JwtAuthenticationFilter;
+
+@Configuration
+public class RouteConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+    private static final String SEGM_STRING = "/${segment}";
+
+    public RouteConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()  
+            .route("route-management", r -> r
+                .path("/api/route-management/**")
+                .filters(f -> f
+                    .filter(jwtFilter) 
+                    .rewritePath("/api/route-management/(?<segment>.*)", SEGM_STRING   )
+                )
+                .uri("lb://gtu-route-management-service"))
+            
+            .route("user-management", r -> r
+                .path("/api/user-management/**")
+                .filters(f -> f
+                    .filter(jwtFilter) 
+                    .rewritePath("/api/user-management/(?<segment>.*)", SEGM_STRING)
+                )
+                .uri("lb://gtu-users-management-service"))
+            .route("auth", r -> r
+                .path("/api/auth/**")
+                .filters(f -> f
+                    .rewritePath("/api/auth/(?<segment>.*)", SEGM_STRING)
+                )
+                .uri("lb://gtu-auth-service"))
+            .build();
+    }
+}
---
+++ src/main/java/com/gtu/api_gateway/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.gtu.api_gateway.security;
+
+import java.util.List;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.gtu.api_gateway.services.JwtService;
+
+import reactor.core.publisher.Mono;
+
+
+
+@Component
+public class JwtAuthenticationFilter implements GatewayFilter {
+
+    private final JwtService jwtService;
+
+
+    private final List<String> excludedPaths = List.of(
+            "/api/auth",
+            "/swagger-ui",
+            "/v3/api-docs",
+            "/swagger-resources"
+    );
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        String path = exchange.getRequest().getPath().value();
+        System.out.println("Request Path: " + path);
+        System.out.println("Authorization Header: " + authHeader);
+        for (String excludedPath : excludedPaths) {
+            if (path.contains(excludedPath)) {
+                return chain.filter(exchange);
+            }
+        }
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+        if (!jwtService.isTokenValid(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+}
