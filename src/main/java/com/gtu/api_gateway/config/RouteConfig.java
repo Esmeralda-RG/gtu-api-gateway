461c96b91e721e39d6d2b059d6b903cee8894805
code3743 <jota.lopez@correounivalle.edu.co>
2025-06-18 10:31:15 -0500

    Update driver-tracker websocket route to support path variable

--- src/main/java/com/gtu/api_gateway/config/RouteConfig.java
+++ src/main/java/com/gtu/api_gateway/config/RouteConfig.java
@@ -57,7 +57,7 @@ public class RouteConfig {
                 )
                 .uri("lb://gtu-driver-tracker"))
             .route("driver-tracker-websocket", r -> r
-                .path("/ws/tracking")
+                .path("/ws/tracking/**")
                 .uri("lb://gtu-driver-tracker"))
             .build();
     }
